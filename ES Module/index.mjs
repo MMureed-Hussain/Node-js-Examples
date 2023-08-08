75d06901a9651d6f7ec9aebca6d56c12cfcd0067
MMureed-Hussain <makilmureedh@gmail.com>
2023-08-08 16:11:43 -0700

    ES Module With Math Example

---
+++ ES Module/index.mjs
@@ -0,0 +1,8 @@
+// import Math from './Math.mjs'; or
+// import * as Math from './Math.mjs'; or
+import {add, sub} from './Math.mjs';
+
+// const {add, sub} = Math;
+
+console.warn('add two no :',add(15,17))
+console.warn("Subtract two no:",sub(17,5))
