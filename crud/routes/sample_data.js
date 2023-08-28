c6f1505246bb1b168c562f6fbf2a78fbed8e0863
MMureed-Hussain <malikmureedh@gmail.com>
2023-08-28 14:47:11 +0500

    Create SQL Connection and Fetech data from sql table

--- crud/routes/sample_data.js
+++ crud/routes/sample_data.js
@@ -1,12 +1,41 @@
-var express = require("express");
+// var express = require("express");
+// var router = express.Router();
+// // var database = require('../database');
+// var database = require('../routes/database')
+
+// router.get("/", function (req, res) {
+//   res.send("List of All Sample data");
+// });
+
+// router.get("/add", function (req, res) {
+//   res.send("Add Sample data Insert");
+// });
+
+// module.exports = router;
+
+var express = require('express');
+
 var router = express.Router();
 
-router.get("/", function (req, res) {
-  res.send("List of All Sample data");
-});
+var database = require('../routes/database')
 
-router.get("/add", function (req, res) {
-  res.send("Add Sample data Insert");
+router.get("/", function(request, response){
+
+	var query = "SELECT * FROM sample_data ORDER BY id DESC";
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error; 
+		}
+		else
+		{
+			response.render('sample_data', {title:'Node.js MySQL CRUD Application', action:'list', sampleData:data});
+		}
+
+	});
+
 });
 
 module.exports = router;
